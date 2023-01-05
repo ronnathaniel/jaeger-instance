539902a7eb89a796264ef581cc00e4503f619d71
ronnathaniel <rnathaniel7@gmail.com>
2023-01-05 13:26:02 -0500

    add `start-jaeger.sh`

---
+++ start-jaeger.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+JAEGER_TAG=${JAEGER_TAG:-1.35}
+
+# jaeger ui is at 16686
+
+docker run --rm -it --name jaeger \
+  -e COLLECTOR_ZIPKIN_HOST_PORT=:9411 \
+  -p 5775:5775/udp \
+  -p 6831:6831/udp \
+  -p 6832:6832/udp \
+  -p 5778:5778 \
+  -p 16686:16686 \
+  -p 14268:14268 \
+  -p 14250:14250 \
+  -p 9411:9411 \
+  jaegertracing/all-in-one:${JAEGER_TAG}
