3f45681e933c8984cc4a2e6b602867d42f0b0854
ronnathaniel <rnathaniel7@gmail.com>
2023-01-24 21:06:39 -0500

    fix: ports

--- start-jaeger.sh
+++ start-jaeger.sh
@@ -10,18 +10,23 @@ docker run \
   --name jaeger \
   -e COLLECTOR_OTLP_ENABLED=true \
   -e COLLECTOR_ZIPKIN_HOST_PORT=:9411 \
-  -e ES_SERVER_URLS=http://localhost:9200 \
-  -e ES_SNIFFER=false \
   -e JAEGER_REPORTER_LOG_SPANS=true \
-  -p 5775:5775/udp \
   -p 6831:6831/udp \
   -p 6832:6832/udp \
+  -p 5775:5775/udp \
   -p 5778:5778 \
   -p 16686:16686 \
-  -p 14268:14268 \
+  -p 4317:4317 \
+  -p 4318:4318 \
   -p 14250:14250 \
+  -p 14268:14268 \
+  -p 14269:14269 \
   -p 9411:9411 \
-  jaegertracing/all-in-one:${JAEGER_TAG}
+  jaegertracing/all-in-one:${JAEGER_TAG} 
+  --log-level=debug
 
 
-#   -e SPAN_STORAGE_TYPE=elasticsearch \
+
+# -e SPAN_STORAGE_TYPE=elasticsearch \
+# -e ES_SERVER_URLS=http://localhost:9200 \
+# -e ES_SNIFFER=false \
