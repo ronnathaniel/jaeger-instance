ffa864c0ba8f476831658b63009d485c37e0dc77
ronnathaniel <rnathaniel7@gmail.com>
2023-01-27 16:30:52 -0500

    fix: ports

--- start-jaeger.sh
+++ start-jaeger.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-JAEGER_TAG=${JAEGER_TAG:-1.35}
+JAEGER_TAG=${JAEGER_TAG:-1.41}
 
 # jaeger ui is at 16686
 
@@ -9,18 +9,14 @@ docker run \
   -it \
   --name jaeger \
   -e COLLECTOR_OTLP_ENABLED=true \
-  -e COLLECTOR_ZIPKIN_HOST_PORT=:9411 \
   -e JAEGER_REPORTER_LOG_SPANS=true \
   -p 6831:6831/udp \
   -p 6832:6832/udp \
-  -p 5775:5775/udp \
-  -p 5778:5778 \
+  -p 16685:16685 \
   -p 16686:16686 \
   -p 4317:4317 \
   -p 4318:4318 \
-  -p 14250:14250 \
   -p 14268:14268 \
-  -p 14269:14269 \
   -p 9411:9411 \
   jaegertracing/all-in-one:${JAEGER_TAG} \
   --log-level=debug
